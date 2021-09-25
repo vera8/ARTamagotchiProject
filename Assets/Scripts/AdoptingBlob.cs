d27ff251b67ae1e15e737e4feb0a46cf448fe18f
Axel G <axel-gauthier@hotmail.fr>
2021-09-25 14:22:45 +0200

    feat: adoption screen

---
+++ Assets/Scripts/AdoptingBlob.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d67bb44c4ebee1626a73a9dbc919a22563d0a521bb0a40d6d0bd81b5a194b9e4
+size 434
---
+++ Assets/Scripts/Blob.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:73500580d1adb218dda2adb4b6a4593272a9c53a473ca7ae4e3b34f83639c443
+size 771
