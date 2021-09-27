f5b3bf503bcff9dc2230f1ccf33416f9a251d5dc
Axel G <axel-gauthier@hotmail.fr>
2021-09-27 13:52:49 +0200

    feat: hunger and emotion status

---
+++ Assets/Scripts/ChangeMood.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef75dfc0cb4fb3d93cfcd1cd96b642e7208cef3c66bbb393e9dbab7fcaf43c83
+size 1047
---
+++ Assets/Scripts/HungerStatus.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:628d31a38f94a057629b0f3881e54ae17084e2b8d58cdda0bec3a088e46f605c
+size 559
