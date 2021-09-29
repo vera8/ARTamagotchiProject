6c926a36b761f4f14a01d8af245f81b00eb8a337
Vera <vera.brennecke@stud.hs-hannover.de>
2021-09-29 18:35:04 +0200

    add some sound effects

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e3c2ebec1a9a25fc567bbcc505395936cf4cb2f0b264d9c30300bcec14b972b
-size 3007
+oid sha256:ec0a68eeba5f1169337b15fab885a177959b20d3d2b696df86fe903bb96b13b4
+size 4030
