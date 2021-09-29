f90bd3a072c683970320248ceba116a175e70e6e
Vera <vera.brennecke@stud.hs-hannover.de>
2021-09-30 00:23:43 +0200

    death scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b35c976d71ecaa872e57d3e7cca6b5cfc7b9824fecbd7ad655870461b67ad414
-size 1183
+oid sha256:10c3c8d4b559f3569557de4efe4b6a4a716f784ef6b20a0ce9d57c29f17a0398
+size 1453
---
+++ Assets/Scripts/Tombstone.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b81e4dc3302f4599ba69e82694f21ed067529a7306e4e8485d0ebb87483712a3
+size 738
